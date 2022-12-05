25b097717ae8c71fac411c1a4d56f11a07b1e4b4
Vaishnav Vijayan <vaishnavtdy@gmail.com>
2022-12-05 17:06:23 +0530

    commit

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "assetdb"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "Admin123"),
+        schema: env("DATABASE_SCHEMA", "public"), // Not Required
+        ssl: false,
+      },
+      options: {},
+    },
+  },
+});
