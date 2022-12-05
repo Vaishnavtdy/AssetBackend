2be4f0bfed013fcac4d96f1fdcd53c7aaa542481
Vaishnav Vijayan <vaishnavtdy@gmail.com>
2022-12-05 18:48:14 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
+        port: env.int("DATABASE_PORT", 5431),
         database: env("DATABASE_NAME", "assetdb"),
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "Admin123"),
